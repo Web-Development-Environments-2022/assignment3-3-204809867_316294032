44d259dec4c68d25f4b6a57457be5b139da53bd4
Darabu273 <dAr8194765>
2022-06-22 21:11:12 +0300

    add nav-About

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
+      <router-link :to="{ name: 'main' }">Main</router-link>|
       <router-link :to="{ name: 'search' }">Search</router-link>|
-      {{ !$root.store.username }}
+      <router-link :to="{ name: 'about' }">About</router-link>|
+      <!--
+         <b-nav-item-dropdown text="Personal" left>
+        <b-dropdown-item href="#">my recipes</b-dropdown-item>
+        <b-dropdown-item href="#">traditional</b-dropdown-item>
+        <b-dropdown-item href="#">Favorites</b-dropdown-item>
+      </b-nav-item-dropdown> -->
+      
+      <!-- {{ !$root.store.username }} this is true when your guest-->
       <span v-if="!$root.store.username">
-        Guest:
+        hello guest:
         <router-link :to="{ name: 'register' }">Register</router-link>|
         <router-link :to="{ name: 'login' }">Login</router-link>|
       </span>
       <span v-else>
+        <!-- username connect this show his name -->
         {{ $root.store.username }}: <button @click="Logout">Logout</button>|
       </span>
     </div>
